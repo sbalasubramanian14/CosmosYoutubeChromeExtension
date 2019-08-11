a0a6c59b4d7a463b9dad6539319d9e2bbe42869c
Bala <sbalasubramanian96@gmail.com>
2019-08-11 18:03:31 +0530

    added github link

--- js/popup.js
+++ js/popup.js
@@ -5,4 +5,14 @@ document.addEventListener("DOMContentLoaded", function() {
       chrome.tabs.create({ url: tab.url.replace(/www./, "ss") });
     });
   });
+
+  var github = document.getElementById("github-link");
+  github.addEventListener("click", function() {
+    chrome.tabs.getSelected(null, function(tab) {
+      chrome.tabs.create({
+        url:
+          "https://github.com/sbalasubramanian14/CosmosYoutubeChromeExtension"
+      });
+    });
+  });
 });
